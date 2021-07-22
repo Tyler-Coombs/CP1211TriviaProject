19b10f475fdd2c0292135f6be17f59d2a7f3dece
Tyler-Coombs <84094311+Tyler-Coombs@users.noreply.github.com>
2021-07-22 21:11:32 -0230

    Added more CSS and JS functionality

--- Tree Trivia/trivia.js
+++ Tree Trivia/trivia.js
@@ -21,8 +21,10 @@
                     );
                 }
                 output.push (
-                    `<div class="question">${currentQuestion.question}</div>,
-                    <div class="answers">${answers.join('')}</div>`
+                    `<div class="slide">
+                        <div class="question">${currentQuestion.question}</div>
+                        <div class="answers">${answers.join('')}</div>
+                    </div>`
                 );
             }
         );
@@ -51,10 +53,36 @@
         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
 
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if(currentSlide === 0){
+          previousButton.style.display = 'none';
+        }
+        else{
+          previousButton.style.display = 'inline-block';
+        }
+        if(currentSlide === slides.length-1){
+          nextButton.style.display = 'none';
+          submitButton.style.display = 'inline-block';
+        }
+        else{
+          nextButton.style.display = 'inline-block';
+          submitButton.style.display = 'none';
+        }
+      }
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+      }
+    
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+      }
+
     const triviaContainer = document.getElementById("trivia");
     const resultsContainer = document.getElementById("results");
     const submitButton = document.getElementById("submit");
-
     const myQuestions = [
         {
             question: "What is the tallest tree in the world?",
@@ -191,9 +219,17 @@
     //display trivia
     buildQuiz();
 
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(currentSlide);
+
     //on submit, show results
-    document.addEventListener("DOMContentLoaded", () => {
-        submitButton.addEventListener("click", showResults);
-    })
+
+    submitButton.addEventListener("click", showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
     
 })();
